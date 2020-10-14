02d1e10713ef933112c87181011705b2981d110e
Yaros <yaros@Computer.lan>
2020-10-14 17:54:15 +0200

    articles/comment classes

---
+++ models/Article.js
@@ -0,0 +1,15 @@
+class Article {
+    constructor(title, subtitle, body, status, tags, creationDate, author, onTop, image){
+        this.title = title;
+        this.subtitle = subtitle;
+        this.body = body;
+        this.tags = tags;
+        this.status = status;
+        this.creationDate = creationDate;
+        this.author = author;
+        this.onTop = onTop;
+        this.image = image;   
+    }
+
+    //get, set
+}
---
+++ models/Comment.js
@@ -0,0 +1,9 @@
+class Comment{
+    constructor(user, commentDate, commentBody){
+        this.user = user;
+        this.commentDate = commentDate;
+        this.commentBody = commentBody;
+    }
+
+    //get, set
+}
