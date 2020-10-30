2f1ad609d201a6ceb5b799ddbc0b8a3f2287ac2c
Yaroslav <yaros2507@gmail.com>
2020-10-30 17:56:54 +0100

    article creator

--- public/js/_main.js
+++ public/js/_main.js
@@ -2,7 +2,7 @@ console.log("loaded script main")
 
 var articlesArr = [] 
 
-articlesArr.push(new Article(true, false, "[]", "r", "r", "r", "r", "r", "r"))
+//articlesArr.push(new Article(true, false, "[]", "r", "r", "r", "r", "r", "r"))
 
 
 
